d8390284e270d2a50e95c080d31dfe453b9b7539
Saumya Raj <rajsaumya7033@gmail.com>
2026-09-19 16:03:49 +0530

    Fix unused imports in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button, ThemeProvider } from '@mui/material';
 import Home from './Customer/Home/Home';
 
 function App() {
-  return (
-    <>
-          <Home>    </Home>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
