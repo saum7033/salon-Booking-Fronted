844520ca883de2aca3aba7e72a34e1f9057950ee
Saumya Raj <rajsaumya7033@gmail.com>
2026-09-20 17:22:10 +0530

    salonpage

---
+++ src/Customer/Salon/Salon Details/SalonDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SalonDetail from './SalonDetail';
+
+const SalonDetails = () => {
+    return(
+        <div className='px-5 lg:px-20'>
+            <SalonDetail/>
+        </div>
+    )
+}
+
+export default SalonDetails;
